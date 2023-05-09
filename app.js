553c2a40304bad1df660a2d52183a67ef1cb4821
Colter Wehmeier <partiform@gmail.com>
2023-05-09 14:22:39 -0500

    test page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
