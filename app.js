ca318baab561e5fcf3a830de92e6c4124558c15e
Chopium <partiform@gmail.com>
2023-05-09 22:01:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'local';
 
-
 // Load config from JSON file
-// const config = JSON.parse(fs.readFileSync('postgres/config/config.local.json', 'utf8'));
 const config = JSON.parse(fs.readFileSync(`postgres/config/config.${env}.json`, 'utf8'));
+console.log(`Loaded config for environment: ${env}`, config);
+
 const pool = new Pool(config.poolConfig);
 
 app.use(express.static('public'));
@@ -16,12 +16,20 @@ app.use(express.static('public'));
 // API endpoint to get a random question
 app.get('/api/question', async (req, res) => {
     try {
+        console.log('Received request for /api/question');
         const result = await pool.query('SELECT * FROM questions ORDER BY RANDOM() LIMIT 1');
+        console.log('Fetched question:', result.rows[0]);
         res.json(result.rows[0]);
     } catch (err) {
-        console.error(err);
+        console.error('Error fetching question:', err);
         res.status(500).json({ error: 'Internal server error' });
     }
+});
+
+// Test endpoint
+app.get('/api/test', (req, res) => {
+    console.log('Received request for /api/test');
+    res.json({ message: 'Test endpoint works!' });
 });
 
 app.listen(port, () => {
