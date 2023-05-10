bae14295a2e21c0098d25cbe04dced72601c777e
Chopium <partiform@gmail.com>
2023-05-09 21:22:33 -0500

    environments

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
-const path = require('path');
-
+const fs = require('fs');
+const { Pool } = require('pg');
 const app = express();
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'local';
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 3000;
+// Load config from JSON file
+// const config = JSON.parse(fs.readFileSync('postgres/config/config.local.json', 'utf8'));
+const config = JSON.parse(fs.readFileSync(`postgres/config/config.${env}.json`, 'utf8'));
+const pool = new Pool(config.poolConfig);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use(express.static('public'));
+
+// API endpoint to get a random question
+app.get('/api/question', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM questions ORDER BY RANDOM() LIMIT 1');
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
